1a0c2a6af50877a9cfbc0d29284245e897fdb78b
Даша <dashasockova@gmail.com>
2025-03-30 15:47:15 +0300

    Решение задач

---
+++ 1. Введение/Задачи/Задача 8.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+x1 = int(input('х1: '))
+y1 = int(input('У1: '))
+x2 = int(input('х2: '))
+y2 = int(input('У2: '))
+print(f'Расстояние между точками ={((x2 - x1)**2 + (y2 - y1)**2)} ')
---
+++ 1. Введение/Задачи/Прямоугольник.py
@@ -0,0 +1,4 @@
+width = int(input('Введите ширину: '))
+height = int(input('Введите длину: '))
+print(f'Площадь = {height * width}')
+print(f'Периметр = {height * 2 + width * 2}')
